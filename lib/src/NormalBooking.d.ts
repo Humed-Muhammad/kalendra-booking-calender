e667c100cd3150ff06ed13f73c15e51f81935360
HumedMohammed <humed@numuw.com>
2025-05-01 19:22:35 +0300

    v69

--- lib/src/NormalBooking.d.ts
+++ lib/src/NormalBooking.d.ts
@@ -1,7 +1,8 @@
-/// <reference types="react" />
+import { type Dispatch, type SetStateAction } from "react";
 import type { BookingProps } from "./types/type";
 interface Props extends BookingProps {
     isLoadingRootEventType: boolean;
+    setLoadingData: Dispatch<SetStateAction<boolean>>;
 }
 export declare const NormalBooking: (props: Props) => JSX.Element;
 export {};
--- lib/src/RoundRobinBooking.d.ts
+++ lib/src/RoundRobinBooking.d.ts
@@ -1,9 +1,10 @@
-/// <reference types="react" />
+import { type Dispatch, type SetStateAction } from "react";
 import { EventType } from "./types";
 import { BookingProps } from "./types/type";
 interface Props extends BookingProps {
     eventType: EventType;
     isLoadingRootEventType: boolean;
+    setLoadingData: Dispatch<SetStateAction<boolean>>;
 }
-export declare const RoundRobinBooking: ({ eventType, ...rest }: Props) => JSX.Element;
+export declare const RoundRobinBooking: ({ eventType, setLoadingData, ...rest }: Props) => JSX.Element;
 export {};
